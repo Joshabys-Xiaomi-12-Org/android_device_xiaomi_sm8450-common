f65b7d4c866785fade1b62805b0573f651b54ca9
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-09-19 17:12:19 -0300

    sm8450-common: init: Switch to deep in mem_sleep

--- rootdir/bin/init.kernel.post_boot-taro.sh
+++ rootdir/bin/init.kernel.post_boot-taro.sh
@@ -212,7 +212,7 @@ else
 	echo N > /sys/devices/system/cpu/qcom_lpm/parameters/sleep_disabled
 fi
 
-echo s2idle > /sys/power/mem_sleep
+echo deep > /sys/power/mem_sleep
 
 # Let kernel know our image version/variant/crm_version
 if [ -f /sys/devices/soc0/select_image ]; then
